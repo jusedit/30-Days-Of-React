87cfb9471915080671593f773b9b2cc3134909f8
Jonas Nabbefeld <jonas@siliconforest.de>
2023-09-13 12:52:10 +0200

    Day 2: What is React Excercise Complete (addendum)

---
+++ solutions/day-02/exercises.js
@@ -0,0 +1,37 @@
+// What is React?
+
+// React is a UI library to create reusable UI components. It handles the interaction with the DOM.
+// The component-based architecture allows to build entire web applications.
+
+// What is a library?
+
+// A collection of pre-written JavaScript Code that makes it easier to develop applications.
+
+// What is a single page application?
+
+// A web application which initially loads a single HTML file.
+// The components of the file are dynamically generated and updated as the user interacts with the website.
+// Note: SPA's can contain more than one HTML file, but typically don't require a full page reload when navigating.
+
+// What is a component ?
+
+// A component is a reusable UI element, that can be composed together to build a web application.
+
+// What is the latest version of React?
+
+// 18.2.0
+
+// What is DOM?
+
+// The Document Object Model is a collection of objects, that represent the content of a website.
+
+// What is React Virtual DOM?
+
+// The React Virtual DOM is a internal representation that React uses to handle the "real" DOM.
+
+// What does a web application or a website(composed of) have?
+
+// A web application or website is typically composed of various components:
+// Front-end: HTML, CSS, JavaScript
+// Assets: Images, videos and other static files
+// Back-end: Databases and server-side code.
