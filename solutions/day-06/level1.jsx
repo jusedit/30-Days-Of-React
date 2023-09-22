7a3742672a9be7019514b3bb66e7897f74959ea9
Jonas Nabbefeld <jonas@siliconforest.de>
2023-09-22 16:19:30 +0200

    Day 6: Level1 complete

---
+++ solutions/day-06/level1.jsx
@@ -0,0 +1,17 @@
+// Why you need to map an array ?
+
+// In React you need to map an array to add HTML elements to the data and return a list of JSX elements
+// Generally mapping allows you to iterate over the elements and apply a function or transformation to each one.
+
+// Why we need keys during mapping an array ?
+
+// Keys help React indentify which corresponding DOM elements have changed, were added or removed
+// Keys should be unique and consistent between renders
+
+// What is the importance of destructuring your code ?
+
+// Destructuring allows to asign values of objects directly to variables, this allows access without repetitive syntaxwrite
+
+// Does destructuring make your code clean and easy to read ?
+
+// Yes, it makes the intentions clear by immediately exposing the variables of interest
